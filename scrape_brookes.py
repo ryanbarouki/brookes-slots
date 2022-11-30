b58eb4f77ff1a8754323a402bb6c08a8534a4f7e
Ryan Barouki <ryanbarouki@gmail.com>
2022-11-30 15:20:26 +0000

    add command to search for slots on a given day

--- scrape_brookes.py
+++ scrape_brookes.py
@@ -4,12 +4,44 @@ from twill.commands import fv as fill_value
 from dotenv import load_dotenv
 import os
 
+class BrookesScraper:
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+
+    def get_slots_for_day(self, day):
+        LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
+        BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
+
+        # login
+        browser.go(LOGIN_URL)
+        fill_value("1", "ctl00$MainContent$InputLogin", self.username)
+        fill_value("1", "ctl00$MainContent$InputPassword", self.password)
+        browser.submit()
+
+        # essentially click "Make a Booking"
+        browser.go(BOOKING_URL)
+
+        # click "Headington Climb"
+        fill_value("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand", "")
+        browser.submit()
+
+        soup = BeautifulSoup(browser.html, "html.parser")
+        return [tag["value"] for tag in soup.find_all("input", value=lambda x: x and x.startswith(day))]
+
+
+
+
+
 if __name__ == "__main__":
     load_dotenv()
-    LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
-    BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
+    # load environment variables from .env file
     USER = os.getenv("BROOKES-USERNAME")
     PASSWORD = os.getenv("BROOKES-PASSWORD")
+
+    LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
+    BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
+
     # login
     browser.go(LOGIN_URL)
     fill_value("1", "ctl00$MainContent$InputLogin", USER)
@@ -23,6 +55,6 @@ if __name__ == "__main__":
     browser.submit()
 
     soup = BeautifulSoup(browser.html, "html.parser")
-    print([tag["id"] for tag in soup.find_all("input", value=lambda x: x and x.startswith("Thurs"))])
+    print([tag["value"] for tag in soup.find_all("input", value=lambda x: x and x.startswith("Thurs"))])
 
     # print(page.text)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,33 @@
-import telegram.ext as tg
+from functools import reduce
 from dotenv import load_dotenv
+import telebot
 import os
+from scrape_brookes import BrookesScraper
 
-def start(update, context):
-    update.message.reply_text("Hello you little climbing slut, missed a Brookes slot have we?")
+load_dotenv()
+API_KEY = os.getenv("TELEGRAM-API-KEY")
+USER = os.getenv("BROOKES-USERNAME")
+PASSWORD = os.getenv("BROOKES-PASSWORD")
+bot = telebot.TeleBot(API_KEY)
 
-if __name__ == "__main__":
-    load_dotenv()
-    token = os.getenv("TELEGRAM-API-KEY")
-    updater = tg.Updater(token, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(tg.CommandHandler('start', start))
-    updater.start_polling()
-    updater.idle()
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hello you little climbing slut, missed a Brookes slot have we?")
+
+def slot_request(message):
+    request = message.text.split()
+    return len(request) >= 2 and request[1].lower() == "slots"
+
+@bot.message_handler(func=slot_request)
+def send_slots(message):
+    day = message.text.split()[0]
+    scraper = BrookesScraper(USER, PASSWORD)
+    slots = scraper.get_slots_for_day(day)
+    if len(slots) == 0:
+        bot.send_message(message.chat.id, "Couldn't find slots!")
+        return
+    slot_string = reduce(lambda a, b: f"{a}\n{b}", slots)
+    bot.send_message(message.chat.id, slot_string)
+
+bot.polling()
+
