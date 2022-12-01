6f775724a3270c97905e31effd6ca7ba4c29ade5
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-01 00:55:31 +0000

    track and schedule a repeated job telling you how many spaces are left for a slot

--- scrape_brookes.py
+++ scrape_brookes.py
@@ -4,24 +4,23 @@ from twill.commands import fv as fill_value
 from dotenv import load_dotenv
 import os
 
+
+def click(form_id, button_id):
+    fill_value(form_id, button_id, "")
+    browser.submit()
 class BrookesScraper:
+    LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
+    BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
+
     def __init__(self, username, password) -> None:
         self.username = username
         self.password = password
 
     def get_slots_for_day(self, day):
-        LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
-        BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
-
-        # login
-        browser.go(LOGIN_URL)
-        fill_value("1", "ctl00$MainContent$InputLogin", self.username)
-        fill_value("1", "ctl00$MainContent$InputPassword", self.password)
-        browser.submit()
+        self.login()
 
         # essentially click "Make a Booking"
-        browser.go(BOOKING_URL)
-
+        browser.go(BrookesScraper.BOOKING_URL)
         # click "Headington Climb"
         click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
 
@@ -34,54 +33,39 @@ class BrookesScraper:
             h4 = soup.find_all('h4', recursive=True)
             tidy_h4_text = [h.get_text().strip() for h in h4]
             dates = list(filter(lambda a: a and a.lower().startswith(day.lower()), tidy_h4_text))
-            slots += dates
+
+            for i in range(len(dates)):
+                button_id = f"ctl00_MainContent_ClassStatus_ctrl{i}_btnAvaliable"
+                status = soup.find('input', id=button_id)['value'].split(" ")
+                if len(status) < 3:
+                    raise Exception("Could not find text in button")
+                if status[0].lower() == "class":
+                    availability = status[2]
+                else:
+                    availability = status[0]
+                
+                slot = {"id": id, "button_id": button_id, "date": dates[i], "status": availability}
+                slots.append(slot)
             # go back
-            browser.go(BOOKING_URL)
+            browser.go(BrookesScraper.BOOKING_URL)
             click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
 
-        return sorted(slots)
+        return sorted(slots, key=lambda x: x['date'])
 
+    def login(self):
+        browser.go(BrookesScraper.LOGIN_URL)
+        fill_value("1", "ctl00$MainContent$InputLogin", self.username)
+        fill_value("1", "ctl00$MainContent$InputPassword", self.password)
+        browser.submit()
 
+    def get_space_count_for_slot(self, slot):
+        self.login()
 
-
-def click(form_id, button_id):
-    fill_value(form_id, button_id, "")
-    browser.submit()
-
-if __name__ == "__main__":
-    load_dotenv()
-    # load environment variables from .env file
-    USER = os.getenv("BROOKES-USERNAME")
-    PASSWORD = os.getenv("BROOKES-PASSWORD")
-
-    LOGIN_URL = "https://brookessport.leisurecloud.net/Book/mrmLogin.aspx"
-    BOOKING_URL = "https://brookessport.leisurecloud.net/Book/mrmselectActivityGroup.aspx"
-
-    # login
-    browser.go(LOGIN_URL)
-    fill_value("1", "ctl00$MainContent$InputLogin", USER)
-    fill_value("1", "ctl00$MainContent$InputPassword", PASSWORD)
-    browser.submit()
-
-    # essentially click "Make a Booking"
-    browser.go(BOOKING_URL)
-    # click "Headington Climb"
-    click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
-
-    soup = BeautifulSoup(browser.html, "html.parser")
-    slots = []
-    for tag in soup.find_all("input", value=lambda x: x and x.startswith("Thurs")):
-        id = tag["id"]
-        click("1", id)
-        soup = BeautifulSoup(browser.html, "html.parser")
-        h4 = soup.find_all('h4', recursive=True)
-        tidy_h4_text = [h.get_text().strip() for h in h4]
-        dates = list(filter(lambda a: a and a.lower().startswith("thu"), tidy_h4_text))
-        slots += dates
-        # go back
-        browser.go(BOOKING_URL)
+        # essentially click "Make a Booking"
+        browser.go(BrookesScraper.BOOKING_URL)
+        # click "Headington Climb"
         click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
-
-    print(sorted(slots))
-
-    # print(page.text)
+        click("1", slot['id'])
+        soup = BeautifulSoup(browser.html, "html.parser")
+        spaces_remaining = soup.find('input', id=slot['button_id'])['value']
+        return spaces_remaining
--- telegram_bot.py
+++ telegram_bot.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import telebot
 import os
 from scrape_brookes import BrookesScraper
+from apscheduler.schedulers.background import BackgroundScheduler
 
 load_dotenv()
 API_KEY = os.getenv("TELEGRAM-API-KEY")
@@ -26,8 +27,28 @@ def send_slots(message):
     if len(slots) == 0:
         bot.send_message(message.chat.id, "Couldn't find slots!")
         return
-    slot_string = reduce(lambda a, b: f"{a}\n{b}", slots)
-    bot.send_message(message.chat.id, slot_string)
+    slot_string = reduce(lambda a, b: f"{a}\n{b}", [f'{i+1}. {slots[i]["date"]} - {slots[i]["status"]}' for i in range(len(slots))])
+    slot_msg = bot.send_message(message.chat.id, slot_string)
+    msg = bot.reply_to(slot_msg, "Which slot do you want to track?")
+    bot.register_next_step_handler(msg, lambda message: process_slot_choice(message, slots))
+
+def process_slot_choice(message, slots):
+    reply = message.text
+    try:
+        slot_id = int(reply) - 1
+        get_availability_for_slot(message, slots[slot_id])
+    except ValueError:
+        msg = bot.reply_to(message, "Not a valid slot choice, please input a number")
+        bot.register_next_step_handler(msg, lambda message: process_slot_choice(message, slots))
+    sched = BackgroundScheduler()
+    sched.add_job(lambda: get_availability_for_slot(message, slots[slot_id]), 'interval', seconds=10)
+    sched.start()
+
+def get_availability_for_slot(message, slot):
+    scraper = BrookesScraper(USER, PASSWORD)
+    space_count = scraper.get_space_count_for_slot(slot)
+    bot.reply_to(message, f"Tracking {slot['date']} slot\nCurrently {space_count.lower()}")
+
 
 bot.polling()
 
