30eac45ec45687ae3890a60cdd7ddd3ccc937e06
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-01 13:52:52 +0000

    improve input and remove jobs that are over

--- scrape_brookes.py
+++ scrape_brookes.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 from twill import browser
 from twill.commands import fv as fill_value
-from dotenv import load_dotenv
-import os
+from datetime import datetime
 
 
 def click(form_id, button_id):
@@ -29,21 +28,10 @@ class BrookesScraper:
         for tag in soup.find_all("input", value=lambda x: x and x.lower().startswith(day.lower())):
             id = tag["id"]
             click("1", id)
-            soup = BeautifulSoup(browser.html, "html.parser")
-            h4 = soup.find_all('h4', recursive=True)
-            tidy_h4_text = [h.get_text().strip() for h in h4]
-            dates = list(filter(lambda a: a and a.lower().startswith(day.lower()), tidy_h4_text))
-
+            dates = self.get_dates(day)
             for i in range(len(dates)):
                 button_id = f"ctl00_MainContent_ClassStatus_ctrl{i}_btnAvaliable"
-                status = soup.find('input', id=button_id)['value'].split(" ")
-                if len(status) < 3:
-                    raise Exception("Could not find text in button")
-                if status[0].lower() == "class":
-                    availability = status[2]
-                else:
-                    availability = status[0]
-                
+                availability = self.get_availability(button_id)
                 slot = {"id": id, "button_id": button_id, "date": dates[i], "status": availability}
                 slots.append(slot)
             # go back
@@ -51,6 +39,30 @@ class BrookesScraper:
             click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
 
         return sorted(slots, key=lambda x: x['date'])
+
+    def get_dates(self, day):
+        soup = BeautifulSoup(browser.html, "html.parser")
+        h4 = soup.find_all('h4', recursive=True)
+        tidy_h4_text = [h.get_text().strip() for h in h4]
+        # add year to string
+        dates = list(filter(lambda a: a and a.lower().startswith(day.lower()), tidy_h4_text))
+        return [self.add_year(date) for date in dates]
+    
+    def add_year(self, date_str):
+        slot_date = datetime.strptime(date_str, f"%a %d %b, %H:%M")
+        if slot_date.month < datetime.now().month:
+            year = datetime.now().year + 1
+        else:
+            year = datetime.now().year
+        slot_date = slot_date.replace(year=year)
+        return datetime.strftime(slot_date, f"%a %d %b %Y, %H:%M")
+
+    def get_availability(self, button_id):
+        soup = BeautifulSoup(browser.html, "html.parser")
+        status = soup.find('input', id=button_id)['value'].split(" ")
+        if len(status) < 3:
+            raise Exception("Could not find text in button")
+        return int(status[0]) if status[0].lower() != "class" else 0
 
     def login(self):
         browser.go(BrookesScraper.LOGIN_URL)
@@ -66,6 +78,9 @@ class BrookesScraper:
         # click "Headington Climb"
         click("1", "ctl00$MainContent$activityGroupsGrid$ctrl1$lnkListCommand")
         click("1", slot['id'])
-        soup = BeautifulSoup(browser.html, "html.parser")
-        spaces_remaining = soup.find('input', id=slot['button_id'])['value']
+        day = slot['date'].split(" ")[0]
+        dates = self.get_dates(day)
+        if slot['date'] not in dates:
+            return 0
+        spaces_remaining = self.get_availability(slot['button_id'])
         return spaces_remaining
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from functools import reduce
 from dotenv import load_dotenv
 import telebot
@@ -10,6 +11,8 @@ API_KEY = os.getenv("TELEGRAM-API-KEY")
 USER = os.getenv("BROOKES-USERNAME")
 PASSWORD = os.getenv("BROOKES-PASSWORD")
 bot = telebot.TeleBot(API_KEY)
+jobs = {}
+tracked_counts = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -21,13 +24,19 @@ def slot_request(message):
 
 @bot.message_handler(func=slot_request)
 def send_slots(message):
-    day = message.text.split()[0]
+    input = message.text.split()[0]
+    print(input)
+    days_of_week = {'monday': 'mon' , 'tuesday': 'tue', 'wednesday': 'wed', 'thursday': 'thu', 'friday': 'fri', 'saturday': 'sat', 'sunday': 'sun'}
+    if input.lower() not in days_of_week:
+        bot.reply_to(message, "Please enter a day of the week. e.g. 'Thursday slots'")
+        return
+    day = days_of_week[input.lower()]
     scraper = BrookesScraper(USER, PASSWORD)
     slots = scraper.get_slots_for_day(day)
     if len(slots) == 0:
         bot.send_message(message.chat.id, "Couldn't find slots!")
         return
-    slot_string = reduce(lambda a, b: f"{a}\n{b}", [f'{i+1}. {slots[i]["date"]} - {slots[i]["status"]}' for i in range(len(slots))])
+    slot_string = reduce(lambda a, b: f"{a}\n{b}", [f'{i+1}. {slots[i]["date"]} - {slots[i]["status"]} spaces' for i in range(len(slots))])
     slot_msg = bot.send_message(message.chat.id, slot_string)
     msg = bot.reply_to(slot_msg, "Which slot do you want to track?")
     bot.register_next_step_handler(msg, lambda message: process_slot_choice(message, slots))
@@ -36,19 +45,32 @@ def process_slot_choice(message, slots):
     reply = message.text
     try:
         slot_id = int(reply) - 1
-        get_availability_for_slot(message, slots[slot_id])
+        tracking_spaces_job(message, slots[slot_id])
     except ValueError:
         msg = bot.reply_to(message, "Not a valid slot choice, please input a number")
         bot.register_next_step_handler(msg, lambda message: process_slot_choice(message, slots))
-    sched = BackgroundScheduler()
-    sched.add_job(lambda: get_availability_for_slot(message, slots[slot_id]), 'interval', seconds=10)
-    sched.start()
+    bot.reply_to(message, f"Tracking {slots[slot_id]['date']} slot")
 
-def get_availability_for_slot(message, slot):
+    sched = BackgroundScheduler()
+    sched.add_job(lambda: tracking_spaces_job(message, slots[slot_id]), 'interval', seconds=10)
+    sched.start()
+    jobs[slots[slot_id]['date']] = sched
+
+def tracking_spaces_job(message, slot):
     scraper = BrookesScraper(USER, PASSWORD)
     space_count = scraper.get_space_count_for_slot(slot)
-    bot.reply_to(message, f"Tracking {slot['date']} slot\nCurrently {space_count.lower()}")
 
+    slot_date = datetime.strptime(slot['date'], f"%a %d %b %Y, %H:%M")
+    if slot_date < datetime.now() and slot['date'] in jobs:
+        jobs[slot['date']].shutdown()
+        del jobs[slot['date']]
+        del tracked_counts[slot['date']]
+
+    if slot['date'] not in tracked_counts:
+        tracked_counts[slot['date']] = space_count
+    if tracked_counts[slot['date']] != space_count:
+        tracked_counts[slot['date']] = space_count
+        bot.send_message(message.chat.id, f"Tracking {slot['date']} slot\nCurrently {space_count} spaces available")
 
 bot.polling()
 
