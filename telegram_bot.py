44ffa5e81583ac748fcff3068333c254fc52a52d
Ryan Barouki <ryanbarouki@gmail.com>
2022-12-03 00:23:50 +0000

    add more stuff to run webhooks

--- telegram_bot.py
+++ telegram_bot.py
@@ -17,16 +17,21 @@ INTERVAL = 10
 URL = f"https://brookes-bot.herokuapp.com/{API_KEY}"
 sched = BackgroundScheduler()
 bot = telebot.TeleBot(API_KEY)
-bot.remove_webhook()
-bot.set_webhook(url=URL)
 tracked_counts_all_chats = {}
 
 app = Flask(__name__)
+@app.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL)
+    return "!", 200
+
 @app.route(f'/{API_KEY}', methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return 'ok',200
+    return "!",200
 
 @bot.message_handler(commands=['reset'])
 def restart(message):
@@ -121,3 +126,6 @@ def tracking_spaces_job(message, slot):
     TRACKING_SPACES = False
 
 sched.start()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
